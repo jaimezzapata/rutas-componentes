f1d141564f1c94457a5e89d2a78ec3094723ce29
jaimezzapata <zapataval2304@gmail.com>
2023-09-20 08:44:14 -0500

    Se crear base de datos local

---
+++ src/components/config/dataBase.jsx
@@ -0,0 +1,12 @@
+let servicios = [
+  {
+    nombre: "Cita clandestina",
+    descripcion:
+      "Programa tu cita random con tu paciente random para que hagas cosas random",
+    ubicacion: "Guayaquil",
+    valor: 50000,
+  },
+  {
+
+  },
+];
